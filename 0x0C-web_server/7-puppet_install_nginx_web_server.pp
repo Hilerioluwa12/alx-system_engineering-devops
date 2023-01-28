f5b028670dae2fe5b136f73e485e322986c52a91
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-01-27 22:48:24 -0800

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# commands in puppet in order to configure our Nginx
+
+exec {'install':
+  command  => 'sudo apt update ; sudo apt -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+exec {'default':
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  provider => shell,
+}
